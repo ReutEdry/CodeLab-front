f14ee04861e6038cb6bc221fc78153664a2a239b
reut <reutedry1@gmail.com>
2024-12-30 15:36:44 +0200

    Add id to codeBock object

--- src/services/code-block.service.local.js
+++ src/services/code-block.service.local.js
@@ -1,10 +1,9 @@
 
 import { storageService } from './async-storage.service'
 import { utilService } from './util.service'
-import { userService } from './user.service'
 
 const STORAGE_KEY = 'codeBlockDB'
-
+_createCodeBlock()
 export const codeBlockService = {
     query,
     getById,
@@ -13,6 +12,8 @@ export const codeBlockService = {
 
 async function query() {
     var codeBlocks = await storageService.query(STORAGE_KEY)
+    console.log(codeBlocks);
+
     return codeBlocks
 }
 
@@ -20,7 +21,20 @@ function getById(codeBlockId) {
     return storageService.get(STORAGE_KEY, codeBlockId)
 }
 
-
+function _createCodeBlock() {
+    let codeBlocks = utilService.loadFromStorage(STORAGE_KEY)
+    if (!codeBlocks || !codeBlocks.length) {
+        codeBlocks = [
+            { _id: utilService.makeId(), subject: 'Loops' },
+            { _id: utilService.makeId(), subject: 'Arrays' },
+            { _id: utilService.makeId(), subject: 'Objects' },
+            { _id: utilService.makeId(), subject: 'HTML5-ES6' },
+            { _id: utilService.makeId(), subject: 'Promises' },
+            { _id: utilService.makeId(), subject: 'Async await' },
+        ]
+        utilService.saveToStorage(STORAGE_KEY, codeBlocks)
+    }
+}
 
 
 
